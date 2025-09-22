02b86e101aaabf0525a157e5cf7c06347736fb68
Nt <natanfrancasantarita@gmail.com>
2025-09-22 18:21:32 +0000

    exemplo 3

--- bd.js
+++ bd.js
@@ -6,41 +6,33 @@ const usuario = {
 
 const dados = [
     {
-        "[
-    {
         "id": 1,
         "titulo": "Mobilidade Urbana BH",
         "descricao": "Explorando as soluções de transporte em Belo Horizonte.",
-
-        "conteudo": "A mobilidade urbana em Belo Horizonte, marcada por congestionamentos e um aumento de veículos individuais, busca soluções com projetos de melhoria da infraestrutura, como a expansão do sistema BRT e a construção de ciclovias, além da inclusão de ônibus elétricos. Há uma discussão sobre a viabilidade da tarifa zero no transporte público, impulsionada por estudos da UFMG, que busca reverter a queda de passageiros nos ônibus e incentivar o transporte coletivo. Investimentos federais de R$ 770 milhões também foram anunciados para obras de mobilidade e drenagem na cidade. ",
-
+        "conteudo": "A mobilidade urbana em Belo Horizonte, marcada por congestionamentos e um aumento de veículos individuais, busca soluções com projetos de melhoria da infraestrutura, como a expansão do sistema BRT e a construção de ciclovias, além da inclusão de ônibus elétricos. Há uma discussão sobre a viabilidade da tarifa zero no transporte público, impulsionada por estudos da UFMG, que busca reverter a queda de passageiros nos ônibus e incentivar o transporte coletivo. Investimentos federais de R$ 770 milhões também foram anunciados para obras de mobilidade e drenagem na cidade.",
         "categoria": "Transporte",
         "autor": "João Silva",
         "data": "2023-09-22",
-        "imagem": "asset/imagem/onibus.png"
+        "imagem": "assets/imagem/onibus.png"
     },
     {
         "id": 2,
         "titulo": "Tecnologia e Inovação nas Cidades do Futuro",
         "descricao": "Como as inovações tecnológicas estão transformando a forma como vivemos nas grandes cidades.",
-
-        "conteudo": "As cidades do futuro, ou cidades inteligentes, serão moldadas pela tecnologia e inovação, focando na sustentabilidade e na melhoria da qualidade de vida dos cidadãos. Isso envolverá o uso de Internet das Coisas (IoT), Inteligência Artificial (IA) e 5G para monitorizar e otimizar serviços urbanos como transporte, energia e gestão de recursos, criando cidades mais eficientes, conectadas e resilientes, como aponta a Bosch no Brasil. ",
+        "conteudo": "As cidades do futuro, ou cidades inteligentes, serão moldadas pela tecnologia e inovação, focando na sustentabilidade e na melhoria da qualidade de vida dos cidadãos. Isso envolverá o uso de Internet das Coisas (IoT), Inteligência Artificial (IA) e 5G para monitorizar e otimizar serviços urbanos como transporte, energia e gestão de recursos, criando cidades mais eficientes, conectadas e resilientes, como aponta a Bosch no Brasil.",
         "categoria": "Tecnologia",
-
         "autor": "Ana Costa",
         "data": "2023-08-15",
-        "imagem": "asset/imagem/tecnologia.png"
+        "imagem": "assets/imagem/tecnologia.png"
     },
     {
         "id": 3,
         "titulo": "O Impacto da Alimentação Saudável no Bem-Estar",
         "descricao": "Explorando como escolhas alimentares podem influenciar a saúde mental e física.",
-
-        "conteudo": "Uma alimentação saudável prioriza alimentos in natura e minimamente processados, como frutas, vegetais, leguminosas, cereais integrais, proteínas magras e gorduras boas, garantindo a ingestão adequada de macronutrientes e micronutrientes. Para ter uma dieta equilibrada, é importante variar a alimentação, planejar as refeições, reduzir o consumo de açúcares e gorduras adicionadas, e evitar alimentos ultraprocessados. A hidratação, o consumo de alimentos da estação, e a atenção a pratos coloridos são também fundamentais para a saúde. ",
-
+        "conteudo": "Uma alimentação saudável prioriza alimentos in natura e minimamente processados, como frutas, vegetais, leguminosas, cereais integrais, proteínas magras e gorduras boas, garantindo a ingestão adequada de macronutrientes e micronutrientes. Para ter uma dieta equilibrada, é importante variar a alimentação, planejar as refeições, reduzir o consumo de açúcares e gorduras adicionadas, e evitar alimentos ultraprocessados. A hidratação, o consumo de alimentos da estação, e a atenção a pratos coloridos são também fundamentais para a saúde.",
         "categoria": "Saúde",
         "autor": "Ricardo Almeida",
         "data": "2023-07-30",
-        "imagem": "asset/imagem/alimentacao.png"
+        "imagem": "assets/imagem/alimentacao.png"
     }
 ];
